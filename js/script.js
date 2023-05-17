557fbd04a8919a21bbd0a7444fdb52ce2d570762
unknown <meervan.datkaeva.ch@gmail.com>
2023-05-17 19:57:13 +0600

    new section slider

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    loop:true,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
